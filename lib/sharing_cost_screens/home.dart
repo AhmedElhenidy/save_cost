591aa4fdab6e34acd2f5cdf76931e217e9e6a308
sahar <sahar.hesham1910@gmail.com>
2022-07-12 15:41:09 +0200

    trip post update 2

--- lib/sharing_cost_screens/home.dart
+++ lib/sharing_cost_screens/home.dart
@@ -30,7 +30,12 @@ class _TripsHomeState extends State<TripsHome> {
                 child: Text('Name',
                   style: Theme.of(context).textTheme.bodyText1,
                 ),) ,
+              subtitle: Text(
+                'Email Address ',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
               trailing: MaterialButton(
+                child: Icon(Icons.more_vert,),
                   onPressed: (){
 
                     showDialog<String>(
@@ -38,14 +43,19 @@ class _TripsHomeState extends State<TripsHome> {
                       builder: (BuildContext context) => AlertDialog(
 
                         title: MaterialButton(
+                          child: Center(child: const Text('Delete'),),
+
+
                             onPressed: (){
+
+
                               Navigator.pop(context);
 
                           showDialog<String>(
                             context:context,
                             builder: (BuildContext context) => AlertDialog(
 
-                              title: const Text('Do you want to delete post'),
+                              title: const Text('Do you want to delete post ?'),
 
                               actions: <Widget>[
                                 TextButton(
@@ -63,7 +73,8 @@ class _TripsHomeState extends State<TripsHome> {
 
 
                         },
-                            child: Center(child: const Text('Delete'))),
+
+                        ),
 
 
                       ),
@@ -71,13 +82,12 @@ class _TripsHomeState extends State<TripsHome> {
                     );
 
 
-
                   },
-                  child: Icon(Icons.more_vert,)),
-              subtitle: Text(
-                'Email Address ',
-                style: Theme.of(context).textTheme.subtitle1,
+
+
+
               ),
+
 
 
 
@@ -225,147 +235,13 @@ class _TripsHomeState extends State<TripsHome> {
               ],),
             ),
             SizedBox(height: 20,),
-            ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.teal,
-                child: Icon(Icons.person,
-                color: Theme.of(context).iconTheme.color,),
-
-              ),
-              title:Container(margin: EdgeInsets.only(top: 10),child:
-              Text('Name',
-                style: Theme.of(context).textTheme.bodyText1,),
-                ) ,
-              trailing: Icon(Icons.list),
-              subtitle: Text('Email Address ',
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
 
 
 
-            ),
-            Padding(
-              padding: const EdgeInsetsDirectional.only(
-                start: 25,
-                end: 25,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text('Place: ',
-                        style: TextStyle(fontSize: 20,
-                          fontStyle:FontStyle.italic,
-                        ),
-                      ),
-                      Text('Cairo',
-                        style: TextStyle(
-                          fontSize: 16,
 
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text('Date:  ',
-                        style: TextStyle(fontSize: 20,
-                          fontStyle:FontStyle.italic,
-                        ),
-                      ),
-                      Text('22-6-2022',
-                        style: TextStyle(
-                          fontSize: 16,
 
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text('Time: ',
-                        style: TextStyle(fontSize: 20,
-                          fontStyle:FontStyle.italic,
-                        ),
-                      ),
-                      Text('11:00 ',
-                        style: TextStyle(
-                          fontSize: 16,
 
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text('Car Model: ',
-                        style: TextStyle(fontSize: 20,
-                          fontStyle:FontStyle.italic,
-                        ),
-                      ),
-                      Text('Honda',
-                        style: TextStyle(
-                          fontSize: 16,
 
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10,),
-
-                  Container(
-                    child: Image(
-                      image: NetworkImage(
-                          'https://imgd.aeplcdn.com/1056x594/n/cw/ec/27074/civic-exterior-right-front-three-quarter-148156.jpeg?q=75&wm=1'),),
-                  )
-
-                ],
-              ),
-            ),
-
-            SizedBox(height: 10,),
-            Padding(
-              padding: const EdgeInsetsDirectional.only(end: 5),
-              child: Row(mainAxisAlignment: MainAxisAlignment.end,
-                children:<Widget> [
-                  Container(
-                    height: 40,
-
-                    color: Colors.teal,
-                    child:
-                    // defaultButton
-                    //   (
-                    //
-                    //   function: ()
-                    //   {
-                    //
-                    //   },
-                    //   text: "Join",
-                    //   radius: 20,
-                    //   width: 100,
-                    // ),
-                    MaterialButton(
-
-                      onPressed: (){},child: Text('check',
-                      style:
-                      TextStyle(color: Colors.white,),
-
-                    ),
-                    ),
-                  ),
-                  SizedBox(width: 20,),
-                  // Container(
-                  //
-                  //   color: Colors.teal,
-                  //   child: MaterialButton(
-                  //     onPressed: (){},child: Text('Chat for details',
-                  //     style: TextStyle(color: Colors.white),
-                  //   ),
-                  //   ),
-                  // ),
-                ],),
-            ),
 
           ],
         ),
